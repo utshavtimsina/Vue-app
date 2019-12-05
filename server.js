bec3505e95c3e835833bea2184ee4a57d5555140
utshavtimsina <utshavtimsina123@gmail.com>
2019-12-05 11:07:20 +0545

    server.js updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
-const port = 3000
+
 
 app.use('/', serveStatic(path.join(__dirname,'/dist')))
 const port = process.env.PORT || 8080
